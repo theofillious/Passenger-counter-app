f698c842afff2e2c6a27864a598c886da8de4559
theofillious <thapelonthorela08@gmail.com>
2024-12-16 03:22:24 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Counting Web App
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Theofillious/pen/bNbgbjB](https://codepen.io/Theofillious/pen/bNbgbjB).
+
---
+++ src/script.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function saved() {
+  let countSeperator = count + " - "
+  saveEl.textContent += countSeperator
+  countEl.textContent = 0
+  count = 0
+}
+
+saved()
+
